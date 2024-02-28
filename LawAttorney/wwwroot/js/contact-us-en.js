dcc2a3b17e1ad7bba0245d16b1e1cdf938aa1137
Yigit Yalcin <yigityalciin@hotmail.com>
2024-02-28 23:32:46 +0300

    captcha added

---
+++ LawAttorney/wwwroot/js/contact-us-en.js
@@ -0,0 +1,13 @@
+﻿document.getElementById('contactForm').addEventListener('submit', function (event) {
+    var recaptchaResponse = grecaptcha.getResponse();
+
+    if (!recaptchaResponse) {
+        event.preventDefault();
+        alert('Please complete the reCAPTCHA.');
+    } else {
+        // Show success message
+        alert('Success! Form submitted.');
+
+        document.getElementById('contactForm').reset();
+    }
+});
---
+++ LawAttorney/wwwroot/js/contact-us-ru.js
@@ -0,0 +1,14 @@
+﻿document.getElementById('contactFormRu').addEventListener('submit', function (event) {
+    var recaptchaResponse = grecaptcha.getResponse();
+
+    if (!recaptchaResponse) {
+        event.preventDefault();
+        alert('Пожалуйста, завершите проверку reCAPTCHA.');
+    } else {
+        // Показать сообщение об успешной отправке
+        alert('Ваше сообщение успешно отправлено.');
+
+        // Опционально, можно сбросить форму после отправки
+        document.getElementById('contactFormRu').reset();
+    }
+});

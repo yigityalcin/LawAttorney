ef439ce66635004831e3b82a2c95726fbbd34610
Yigit Yalcin <yigityalciin@hotmail.com>
2024-03-01 23:37:53 +0300

    v2.0.2

--- LawAttorney/wwwroot/js/language.js
+++ LawAttorney/wwwroot/js/language.js
@@ -1,34 +1,16 @@
-﻿//document.addEventListener('DOMContentLoaded', function () {
-//    var languageDropdown = document.getElementById('languageDropdown');
+﻿// Dil seçildiğinde çağrılacak fonksiyon
+function changeLanguage(lang, text) {
+    // languageText elementini güncelle
+    document.getElementById('languageText').innerText = text;
 
-//    // Önceki dil seçeneğini al
-//    var selectedLanguage = localStorage.getItem('selectedLanguage');
-
-//    // Eğer önceki bir dil seçilmişse, onu aktif hale getir
-//    if (selectedLanguage) {
-//        var activeLanguageElement = languageDropdown.querySelector('.dropdown-toggle');
-//        if (activeLanguageElement) {
-//            activeLanguageElement.innerHTML = selectedLanguage;
-//        }
-//    }
-
-//    // Dil seçeneği tıklandığında
-//    languageDropdown.addEventListener('click', function (event) {
-//        if (event.target.classList.contains('dropdown-item')) {
-//            // Seçilen dilin önceki aktif durumunu kaldır
-//            var previousActiveLanguage = this.querySelector('.dropdown-toggle');
-//            if (previousActiveLanguage) {
-//                previousActiveLanguage.innerHTML = event.target.textContent;
-//            }
-
-//            // Seçilen dil öğesine aktif durumu ekle
-//            var selectedLanguageElement = event.target;
-//            // Seçilen dil değerini al
-//            var selectedLanguage = selectedLanguageElement.getAttribute('data-lang');
-//            // Set the selected language in the dropdown toggle
-//            this.querySelector('.dropdown-toggle').innerHTML = selectedLanguage;
-//            // Set the selected language in localStorage
-//            localStorage.setItem('selectedLanguage', selectedLanguage);
-//        }
-//    });
-//});
+    // Her bir linkin href özelliğini güncelle
+    var links = document.querySelectorAll('.lang-select .dropdown-item');
+    links.forEach(function (link) {
+        var dataLang = link.getAttribute('data-lang');
+        if (dataLang === lang) {
+            link.classList.add('selected'); // Opsiyonel: Seçilen dili vurgulamak için bir class ekleyebilirsiniz.
+        } else {
+            link.classList.remove('selected');
+        }
+    });
+}
